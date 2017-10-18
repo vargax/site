04bd0db2554e18a19e10e9701702650f1348cafe
Juan Pablo <sistemas@bodegasyoficinas.com>
2017-10-17 22:46:17 -0500

    Se corrigió el error de casteo en la clase Gui.java, se agregaron nuevas imagenes

--- src/main/java/co/com/tecni/site/ui/Gui.java
+++ src/main/java/co/com/tecni/site/ui/Gui.java
@@ -1,13 +1,16 @@
 package co.com.tecni.site.ui;
 
+import co.com.tecni.site.lógica.Agrupación;
 import co.com.tecni.site.lógica.ISite;
 import co.com.tecni.site.lógica.Site;
+import co.com.tecni.site.lógica.inmueble.Ficha;
 import co.com.tecni.site.lógica.inmueble.Inmueble;
 
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeCellRenderer;
 import java.awt.*;
 
 public class Gui extends JFrame {
@@ -21,9 +24,7 @@ public class Gui extends JFrame {
 
         //tree = new JTree(tecni);
         tree = new JTree(site);
-        ImageIcon imageIcon = new ImageIcon(Gui.class.getResource("/new.png"));
-        DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
-        renderer.setLeafIcon(imageIcon);
+        NewTreeCellRenderer renderer = new NewTreeCellRenderer();
 
         tree.setCellRenderer(renderer);
         tree.setShowsRootHandles(true);
@@ -34,7 +35,6 @@ public class Gui extends JFrame {
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         splitPane.setLeftComponent(new JScrollPane(tree));
         splitPane.setRightComponent(new JScrollPane(info));
-
         Dimension minimumSize = new Dimension(100, 50);
         tree.setMinimumSize(minimumSize);
         info.setMinimumSize(minimumSize);
@@ -46,8 +46,19 @@ public class Gui extends JFrame {
         tree.getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
             //@Override
             public void valueChanged(TreeSelectionEvent e) {
-                Inmueble inmueble = (Inmueble) tree.getLastSelectedPathComponent();
-                info.setText(inmueble.genNombre());
+                Object o = tree.getLastSelectedPathComponent();
+                if (o instanceof Inmueble) {
+                    Inmueble i = (Inmueble)o;
+                    info.setText(i.toString());
+                }
+                else if (o instanceof Ficha) {
+                    Ficha f = (Ficha)o;
+                    info.setText(f.toString());
+                }
+                else if (o instanceof Agrupación) {
+                    Agrupación a = (Agrupación)o;
+                    info.setText(a.toString());
+                }
             }
         });
 
@@ -55,6 +66,30 @@ public class Gui extends JFrame {
         this.setTitle("SITE");
         this.setSize(800, 600);
         this.setVisible(true);
+    }
+    class NewTreeCellRenderer implements TreeCellRenderer {
+        private JLabel label;
+
+        NewTreeCellRenderer() {
+            label = new JLabel();
+        }
+
+        public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,
+                                                      boolean leaf, int row, boolean hasFocus) {
+            if (value instanceof Inmueble) {
+                label.setIcon(new ImageIcon(Gui.class.getResource("/inmueble.png")));
+                label.setText(((Inmueble)value).genNombre());
+            }
+            else if (value instanceof Ficha) {
+                label.setIcon(new ImageIcon(Gui.class.getResource("/ficha.png")));
+                label.setText(value+"");
+            }
+            else if (value instanceof Agrupación) {
+                label.setIcon(new ImageIcon(Gui.class.getResource("/agrupacion.png")));
+                label.setText(((Agrupación)value).genNombre());
+            }
+            return label;
+        }
     }
 
     public static void main(String[] args) {
