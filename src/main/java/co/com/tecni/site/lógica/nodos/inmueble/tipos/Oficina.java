619802d40c7166a5d58bc2ace67e1d861b6e94d2
gTecnologia <jalbornoz@bodegasyoficinas.com>
2017-12-18 11:51:20 -0500

    Prueba getCapacidad

--- src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/Oficina.java
+++ src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/Oficina.java
@@ -3,9 +3,14 @@ package co.com.tecni.site.lógica.nodos.inmueble.tipos;
 public class Oficina extends _Inmueble {
 
     public final static String SIGLA = "OF";
+    private int capacidadElectrica = 0 ;
 
     public Oficina() {
         super();
         super.sigla = SIGLA;
+    }
+
+    public int getCapacidadElectrica () {
+        return capacidadElectrica;
     }
 }
