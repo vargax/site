b32eb60e271cf90cd56f8c47dbd418053284e68f
cvargasc <cvargasc@activarsas.co>
2018-01-30 22:23:20 -0500

    JSON simple dependency + ajuste atributos inmuebles

--- src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/Local.java
+++ src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/Local.java
@@ -3,44 +3,10 @@ package co.com.tecni.site.lógica.nodos.inmueble.tipos;
 public class Local extends _Inmueble {
 
     public final static String SIGLA = "LC";
-    private double altura = 0 ;
-    private int numeroBaños = 0;
-    private String acabadoPisos = "";
 
     public Local() {
         super();
         super.sigla = SIGLA;
     }
 
-    public double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
-
-    public int getNumeroBaños() {
-        return numeroBaños;
-    }
-
-    public void setNumeroBaños(int numeroBaños) {
-        this.numeroBaños = numeroBaños;
-    }
-
-    public String getAcabadoPisos() {
-        return acabadoPisos;
-    }
-
-    public void setAcabadoPisos(String acabadoPisos) {
-        this.acabadoPisos = acabadoPisos;
-    }
-    @Override
-    public String toString() {
-        return super.toString().substring(0, super.toString().length()-2) +
-                ",altura:" + altura +
-                ",acabadoPisos:'" + acabadoPisos + "'" +
-                ",numeroBaños:" + numeroBaños +
-                " }";
-    }
 }
--- src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/ZonaComún.java
+++ src/main/java/co/com/tecni/site/lógica/nodos/inmueble/tipos/ZonaComún.java
@@ -3,34 +3,10 @@ package co.com.tecni.site.lógica.nodos.inmueble.tipos;
 public class ZonaComún extends _Inmueble {
 
     public final static String SIGLA = "ZC";
-    private int altura = 0;
-    private int numeroBaños = 0;
 
     public ZonaComún() {
         super();
         super.sigla = SIGLA;
     }
 
-    public int getAltura() {
-        return altura;
-    }
-
-    public void setAltura(int altura) {
-        this.altura = altura;
-    }
-
-    public int getNumeroBaños() {
-        return numeroBaños;
-    }
-
-    public void setNumeroBaños(int numeroBaños) {
-        this.numeroBaños = numeroBaños;
-    }
-    @Override
-    public String toString() {
-        return super.toString().substring(0, super.toString().length()-2) +
-                ",altura:" + altura +
-                ",numeroBaños:" + numeroBaños +
-                " }";
-    }
 }
